d0ac0b34b206546d0c679ea45d23bcd32118e757
Conor Luddy <conorluddy@gmail.com>
2016-11-14 09:51:39 +0000

    Kinda works... Needs font embedded and some QA.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,24 +5,57 @@ angular.module('starter.controllers', [])
 
 
 .controller('IncomingBussesCtrl', function($scope, Busses) {
+  let eta = 0;
+  let seconds;
 
-  $scope.$on('$ionicView.enter', function(e) {
-    console.log('x');
-    Busses.get(145, 5127).then((busses)=>{
-      $scope.busses = busses;
+  let update = ()=> {
+    return Busses.get(145, 3148).then((busses)=>{
+      $scope.nextBus = busses[0];
+      $scope.route = busses[0] ? busses[0].route : '';
+      $scope.destination = busses[0] ? busses[0].destination : '';
+
+      eta = moment(busses[0].arrivaldatetime, "DD/MM/YYYY HH:mm:ss");
+      seconds = eta.diff(moment(), 'seconds');
+
+      $scope.nextBus.due = {
+        h: eta.format('HH'),
+        m: eta.format('mm'),
+        s: eta.format('ss')
+      };
+
+      $scope.nextBus.til = Math.floor(seconds/60) + ':' + (seconds%60 > 9 ? seconds%60 : '0' + seconds%60);
+
+      etaAfter = moment(busses[1].arrivaldatetime, "DD/MM/YYYY HH:mm:ss");
+
+      $scope.etaNextDiff = Math.floor(etaAfter.diff(eta) / 60000);
     });
-  });
+  };
 
 
-  $scope.doRefresh = function() {
-      console.log('Refreshing');
-      Busses.get(145, 5127).then((busses)=>{
-        $scope.busses = busses;
-      }).finally(function() {
-         // Stop the ion-refresher from spinning
-         $scope.$broadcast('scroll.refreshComplete');
-       });
-    };
+  let timeDisplay = ()=> {
+    if (eta._isAMomentObject) {
+
+      $scope.$apply(()=>{
+        seconds = eta.diff(moment(), 'seconds');
+        $scope.nextBus.til = Math.floor(seconds/60) + ':' + (seconds%60 > 9 ? seconds%60 : '0' + seconds%60);
+      });
+    }
+  };
+
+
+  $scope.$on('$ionicView.enter', update);
+  $scope.doRefresh = () => {
+    update().finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+     });
+  };
+
+
+  // let refreshCountdown =
+  setInterval(update, 15 * 1000);
+  // let timeCountdown =
+  setInterval(timeDisplay, 1000);
 
 })
 
@@ -30,18 +63,58 @@ angular.module('starter.controllers', [])
 
 
 .controller('HomeBussesCtrl', function($scope, Busses) {
+  let eta = 0;
+  let seconds;
 
-  $scope.$on('$ionicView.enter', function(e) {
-    console.log('y');
-    Busses.get(145, 1478).then((busses)=>{
-      $scope.busses = busses;
+  let update = ()=> {
+    return Busses.get(145, 1478).then((busses)=>{
 
-      console.log(busses[0]);
-
+      $scope.nextBus = busses[0];
       $scope.route = busses[0] ? busses[0].route : '';
       $scope.destination = busses[0] ? busses[0].destination : '';
+
+      eta = moment(busses[0].arrivaldatetime, "DD/MM/YYYY HH:mm:ss");
+      seconds = eta.diff(moment(), 'seconds');
+
+      $scope.nextBus.due = {
+        h: eta.format('HH'),
+        m: eta.format('mm'),
+        s: eta.format('ss')
+      };
+
+      $scope.nextBus.til = Math.floor(seconds/60) + ':' + (seconds%60 > 9 ? seconds%60 : '0' + seconds%60);
+
+      etaAfter = moment(busses[1].arrivaldatetime, "DD/MM/YYYY HH:mm:ss");
+
+      $scope.etaNextDiff = Math.floor(etaAfter.diff(eta) / 60000);
     });
-  });
+  };
+
+
+  let timeDisplay = ()=> {
+    if (eta._isAMomentObject) {
+
+      $scope.$apply(()=>{
+        seconds = eta.diff(moment(), 'seconds');
+        $scope.nextBus.til = Math.floor(seconds/60) + ':' + (seconds%60 > 9 ? seconds%60 : '0' + seconds%60);
+      });
+    }
+  };
+
+
+  $scope.$on('$ionicView.enter', update);
+  $scope.doRefresh = () => {
+    update().finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+     });
+  };
+
+
+  // let refreshCountdown =
+  setInterval(update, 15 * 1000);
+  // let timeCountdown =
+  setInterval(timeDisplay, 1000);
 
 })
 
--- www/js/services.js
+++ www/js/services.js
@@ -6,24 +6,20 @@ angular.module('starter.services', [])
     all: function() {
       var url = 'https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=5127&routeid=145&format=json';
       return $http.get(url).then((response) => {
-        console.log(response.data);
-        console.log(response.data.results);
-
+        // console.log(response.data);
+        // console.log(response.data.results);
         return response.data.results;
-
       }).catch(function(response){
         // console.log(response);
       });
-      // return busses;
+
     },
     get: function(routeId, stopId) {
       var url = 'https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid='+stopId+'&routeid='+routeId+'&format=json';
       return $http.get(url).then((response) => {
-        console.log(response.data);
-        console.log(response.data.results);
-
+        // console.log(response.data);
+        // console.log(response.data.results);
         return response.data.results;
-
       }).catch(function(response){
         // console.log(response);
       });
@@ -58,3 +54,10 @@ angular.module('starter.services', [])
     // }
   };
 });
+
+
+
+
+
+
+// {"errorcode":"0","errormessage":"","numberofresults":3,"stopid":"1478","timestamp":"13\/11\/2016 19:51:26","results":[{"arrivaldatetime":"13\/11\/2016 20:05:11","duetime":"13","departuredatetime":"13\/11\/2016 20:05:11","departureduetime":"13","scheduledarrivaldatetime":"13\/11\/2016 20:05:00","scheduleddeparturedatetime":"13\/11\/2016 20:05:00","destination":"Ballywaltrim","destinationlocalized":"Baile Bhaltraim","origin":"Heuston Station","originlocalized":"Stáisiun Heuston","direction":"Outbound","operator":"bac","additionalinformation":"","lowfloorstatus":"no","route":"145","sourcetimestamp":"13\/11\/2016 19:06:29","monitored":"true"},{"arrivaldatetime":"13\/11\/2016 20:25:11","duetime":"33","departuredatetime":"13\/11\/2016 20:25:11","departureduetime":"33","scheduledarrivaldatetime":"13\/11\/2016 20:25:00","scheduleddeparturedatetime":"13\/11\/2016 20:25:00","destination":"Ballywaltrim","destinationlocalized":"Baile Bhaltraim","origin":"Heuston Station","originlocalized":"Stáisiun Heuston","direction":"Outbound","operator":"bac","additionalinformation":"","lowfloorstatus":"no","route":"145","sourcetimestamp":"13\/11\/2016 19:26:17","monitored":"true"},{"arrivaldatetime":"13\/11\/2016 20:44:35","duetime":"53","departuredatetime":"13\/11\/2016 20:44:35","departureduetime":"53","scheduledarrivaldatetime":"13\/11\/2016 20:45:00","scheduleddeparturedatetime":"13\/11\/2016 20:45:00","destination":"Ballywaltrim","destinationlocalized":"Baile Bhaltraim","origin":"Heuston Station","originlocalized":"Stáisiun Heuston","direction":"Outbound","operator":"bac","additionalinformation":"","lowfloorstatus":"no","route":"145","sourcetimestamp":"13\/11\/2016 19:46:05","monitored":"true"}]}
